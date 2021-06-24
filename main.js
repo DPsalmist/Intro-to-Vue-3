214fe98228c891dc87559b7738ca0d3f5f4c3f8d
Samuel Damilare <sdamilare420@gmail.com>
2021-06-24 13:11:20 +0100

    Event handling completed

--- main.js
+++ main.js
@@ -7,9 +7,25 @@ const app = Vue.createApp({
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 1233, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 1234, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
-    }
+    },
+    // Methods
+    methods:{
+        // addToCart Method
+        addToCart() {
+            this.cart += 1;
+        },
+        // removeFromCart Method
+        removeFromCart() {
+            this.cart -= 1;
+        },
+         //Update Image Method
+        updateImage(variantImg) {
+            this.image = variantImg
+        }
+    },
+   
 })
