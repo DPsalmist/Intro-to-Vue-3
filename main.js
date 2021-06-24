507d40219eec2b6c6a9a84f7a409be41fcbdff2d
Samuel Damilare <sdamilare420@gmail.com>
2021-06-24 11:23:14 +0100

    List Rendering Completed

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 123, color: 'blue'},
+                {id: 124, color: 'green'},
+            ],
+            sizes: [
+                {id: 123, size: 'large, medium'},
+                {id: 124, size: 'large, small'},
+            ]
         }
     }
 })
